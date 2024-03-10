2350649cba3b027e231979fa9021e78c48d55597
gabita142003 <gabita142003@gmail>
2024-03-09 18:22:44 -0600

    Segunda versión de generar getter,setter y constructor

--- src/archivo.java
+++ src/archivo.java
@@ -1,14 +1,11 @@
-
-public class archivo  {
+ public class archivo {
+	private String nomArchivo;
+	private String contenido;
+	private String nomUsuario;
+	private String fechaCrea;
+	private String formato;
+	private String tamaño;
 	
-		private String nomArchivo;
-		private String contenido;
-		private String nomUsuario;
-		private String fechaCrea;
-		private String formato;
-		private String tamaño;
-		
-		
 	public archivo(String nomArchivo, String contenido, String nomUsuario, String fechaCrea, String formato, String tamaño) {
 		this.nomArchivo = nomArchivo;
 		this.contenido = contenido;
@@ -16,48 +13,47 @@ public class archivo  {
 		this.fechaCrea = fechaCrea;
 		this.formato = formato;
 		this.tamaño = tamaño;
-		
-		
-		}
 
-		public String getNomArchivo() {
-		return nomArchivo;
-		}
-		public void setNomArchivo(String nomArchivo) {
-		this.nomArchivo = nomArchivo;
-		}
-		public String getContenido() {
-		return contenido;
-		}
-		public void setContenido(String contenido) {
-		this.contenido = contenido;
-		}
-		public String getNomUsuario() {
-		return nomUsuario;
-		}
-		public void setNomUsuario(String nomUsuario) {
-		this.nomUsuario = nomUsuario;
-		}
-		public String getFechaCrea() {
-		return fechaCrea;
-		}
-		public void setFechaCrea(String fechaCrea) {
-		this.fechaCrea = fechaCrea;
-		}
-		public String getFormato() {
-		return formato;
-		}
-		public void setFormato(String formato) {
-		this.formato = formato;
-		}
-		public String getTamaño() {
-		return tamaño;
-		}
-		public void setTamaño(String tamaño) {
-		this.tamaño = tamaño;
-		
-		 		 
-     }
+	}
+	
+	// TODO Auto-generated constructor stub
 
+	public String getNomArchivo() {
+	return nomArchivo;
+	}
+	public void setNomArchivo(String nomArchivo) {
+	this.nomArchivo = nomArchivo;
+	}
+	public String getContenido() {
+	return contenido;
+	}
+	public void setContenido(String contenido) {
+	this.contenido = contenido;
+	}
+	public String getNomUsuario() {
+	return nomUsuario;
+	}
+	public void setNomUsuario(String nomUsuario) {
+	this.nomUsuario = nomUsuario;
+	}
+	public String getFechaCrea() {
+	return fechaCrea;
+	}
+	public void setFechaCrea(String fechaCrea) {
+	this.fechaCrea = fechaCrea;
+	}
+	public String getFormato() {
+	return formato;
+	}
+	public void setFormato(String formato) {
+	this.formato = formato;
+	}
+	public String getTamaño() {
+	return tamaño;
+	}
+	public void setTamaño(String tamaño) {
+	this.tamaño = tamaño;
+
+	 }
 
 }
