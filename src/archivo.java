6d712bb802e192aa3e8214bc85740d78ef6cbf4b
gabita142003 <gabita142003@gmail>
2024-03-09 18:00:55 -0600

    Quinta versión "Diseño de interfaz y clase"

--- src/archivo.java
+++ src/archivo.java
@@ -1,5 +1,71 @@
 
 public class archivo  {
 	
+		private String nomArchivo;
+		private String contenido;
+		private String nomUsuario;
+		private String fechaCrea;
+		private String formato;
+		private String tamaño;
+		private int ordenPrioridad; //Atributo que nos ayudara a mantener en orden la cola  
+		//Orden de prioridad: 1-Si el archvio es urgente ,2-Si el archivo es importante pero no tan urgente ,3- Programado para una fecha en especifica
+		
+	public archivo(String nomArchivo, String contenido, String nomUsuario, String fechaCrea, String formato, String tamaño, int ordenPrioridad) {
+		this.nomArchivo = nomArchivo;
+		this.contenido = contenido;
+		this.nomUsuario = nomUsuario;
+		this.fechaCrea = fechaCrea;
+		this.formato = formato;
+		this.tamaño = tamaño;
+		this.ordenPrioridad= ordenPrioridad; // 1- Urgente, 2-Pendiente , 3-No importante 
+		
+		}
+		
+		// TODO Auto-generated constructor stub
+
+		public String getNomArchivo() {
+		return nomArchivo;
+		}
+		public void setNomArchivo(String nomArchivo) {
+		this.nomArchivo = nomArchivo;
+		}
+		public String getContenido() {
+		return contenido;
+		}
+		public void setContenido(String contenido) {
+		this.contenido = contenido;
+		}
+		public String getNomUsuario() {
+		return nomUsuario;
+		}
+		public void setNomUsuario(String nomUsuario) {
+		this.nomUsuario = nomUsuario;
+		}
+		public String getFechaCrea() {
+		return fechaCrea;
+		}
+		public void setFechaCrea(String fechaCrea) {
+		this.fechaCrea = fechaCrea;
+		}
+		public String getFormato() {
+		return formato;
+		}
+		public void setFormato(String formato) {
+		this.formato = formato;
+		}
+		public String getTamaño() {
+		return tamaño;
+		}
+		public void setTamaño(String tamaño) {
+		this.tamaño = tamaño;
+		}
+		public int getOrdenPrioridad() {
+			return ordenPrioridad;
+	    }
+		public void  setOrdenPrioridad(int ordenPrioridad) {
+			this.ordenPrioridad=ordenPrioridad;
+		 		 
+     }
+
 
 }
